de5b52b95fde34d0873537841deed726e5cf5c94
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-07-13 21:43:28 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,54 @@
+//----------MOSTRAR O MENU----------
+
+const showMenu = (toggleId, navId) =>{
+	const toggle = document.getElementById(toggleId),
+	nav = document.getElementById(navId)
+
+	if(toggle && nav) {
+		toggle.addEventListener('click', ()=>{
+			nav.classList.toggle('show-menu')
+		})
+	}
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+
+// -------Sumir com o menu quando clicado----------
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+        navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//-----------DEIXAR O LINK ATIVO EM DESTAQUE----------
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+//--------ADICIONAR SOMBRA QUANDO ROLADA A PÁGINA----------
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
